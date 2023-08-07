897709f4a591567c574af2f38adeccf32983ee6d
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-08-07 22:32:26 +0300

    Update SentenceTokenizerCommandTest.php

--- tests/Feature/SentenceTokenizerCommandTest.php
+++ tests/Feature/SentenceTokenizerCommandTest.php
@@ -9,13 +9,19 @@ class SentenceTokenizerCommandTest extends TestCase
 {
     public function test_tokenize_sentence_with_file()
     {
-        $this->artisan('tokenizer:sentence', ['file' => 'tests\fixture\en-tokenize-sentence.test'])
+        $this->artisan('tokenizer:sentence', ['input-file' => 'tests\fixture\en-tokenize-sentence.test'])
             ->expectsOutput(File::get('tests\fixture\en-tokenize-sentence.output'));
     }
 
-    public function test_tokenize_sentence_with_no_file()
+    public function test_tokenize_sentence_without_input_file()
     {
-        $this->expectExceptionMessage('Not enough arguments (missing: "file").');
+        $this->expectExceptionMessage('Not enough arguments (missing: "input-file").');
+        $this->artisan('tokenizer:sentence');
+    }
+
+    public function test_tokenize_sentence_without_output_file()
+    {
+        $this->expectExceptionMessage('Not enough arguments (missing: "output-file").');
         $this->artisan('tokenizer:sentence');
     }
 }
