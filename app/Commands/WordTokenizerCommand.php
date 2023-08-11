ac908de2250106bc6c9b57f8e474de28326a2c72
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-08-11 12:01:11 +0300

    started word tokenizer

---
+++ app/Commands/WordTokenizerCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Support\Facades\File;
+use LaravelZero\Framework\Commands\Command;
+use Rubix\ML\Tokenizers\Word;
+
+final class WordTokenizerCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'tokenizer:word
+                            {input-file : Input file (required)}
+                            {output-file : Output file (required)}';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    private string $inputFileContent;
+
+    /** @var list<string> */
+    private array $words = [];
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $this->task('Reading the input file 📖', function (): void {
+            $this->inputFileContent = File::get($this->argument('input-file'));
+        });
+        $this->task('Tokenizing the text 🔨', function (): void {
+            $word = new Word();
+            $this->words = $word->tokenize($this->inputFileContent);
+        }, 'Processing...');
+        $this->task('Writing to the output file ✍️', function (): void {
+            File::put($this->argument('output-file'), implode("\n", $this->words));
+        });
+    }
+}
