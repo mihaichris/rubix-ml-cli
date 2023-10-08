ce7bdea769dec004e69ddf09037b9c9dbcb4e141
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-10-08 16:58:53 +0300

    Create NGramTokenizerCommandTest.php

---
+++ tests/Feature/Commands/NGramTokenizerCommandTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\File;
+
+test('tokenize command will return ok', function () {
+    $this->artisan(
+        'tokenizer:ngram',
+        [
+            'input-file' => 'tests\fixture\en-tokenize-n-gram.input',
+            'output-file' => 'tests\output\test_tokenize_command_will_return_ok.output'
+        ]
+    )->assertOk();
+});
+
+test('tokenize with file', function () {
+    $expected = File::get('tests\fixture\en-tokenize-n-gram.output');
+    $actual = File::get('tests\output\test_tokenize_command_will_return_ok.output');
+    expect($actual)->toBe($expected);
+})->depends('tokenize command will return ok');
+
+test('tokenize without input file', function () {
+    $this->expectExceptionMessage('Not enough arguments (missing: "input-file").');
+    $this->artisan('tokenizer:ngram', ['output-file' => 'tests\output\en-tokenize-n-gram.output']);
+});
+
+test('tokenize without output file', function () {
+    $this->expectExceptionMessage('Not enough arguments (missing: "output-file").');
+    $this->artisan('tokenizer:ngram', ['input-file' => 'tests\fixture\en-tokenize-n-gram.input']);
+});
