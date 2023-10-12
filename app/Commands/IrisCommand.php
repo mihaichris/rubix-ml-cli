9579fa7b3c35b195076005530bfac644e8734bc3
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-10-12 21:27:08 +0300

    Create IrisCommand.php

---
+++ app/Commands/IrisCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+use Rubix\ML\Classifiers\KNearestNeighbors;
+use Rubix\ML\CrossValidation\Metrics\Accuracy;
+use Rubix\ML\Datasets\Labeled;
+use Rubix\ML\Extractors\NDJSON;
+use Rubix\ML\Loggers\Screen;
+
+class IrisCommand extends Command
+{
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'train:iris';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'Iris train command';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $logger = new Screen();
+
+        $logger->info('Loading data into memory');
+
+        $training = Labeled::fromIterator(new NDJSON('tests\fixture\iris-dataset.ndjson'));
+
+        $testing = $training->randomize()->take(10);
+
+        $estimator = new KNearestNeighbors(5);
+
+        $logger->info('Training');
+
+        $estimator->train($training);
+
+        $logger->info('Making predictions');
+
+        $predictions = $estimator->predict($testing);
+
+        $metric = new Accuracy();
+
+        $score = $metric->score($predictions, $testing->labels());
+
+        $logger->info("Accuracy is $score");
+    }
+}
