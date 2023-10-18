7d56eac0e55dfad0268878913210405efeb59825
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-10-18 19:09:26 +0300

    Update HousePricePredictorCommand.php

--- app/Commands/HousePricePredictorCommand.php
+++ app/Commands/HousePricePredictorCommand.php
@@ -3,6 +3,18 @@
 namespace App\Commands;
 
 use LaravelZero\Framework\Commands\Command;
+use Rubix\ML\Datasets\Labeled;
+use Rubix\ML\Extractors\ColumnPicker;
+use Rubix\ML\Extractors\CSV;
+use Rubix\ML\Loggers\Screen;
+use Rubix\ML\PersistentModel;
+use Rubix\ML\Persisters\Filesystem;
+use Rubix\ML\Regressors\GradientBoost;
+use Rubix\ML\Regressors\RegressionTree;
+use Rubix\ML\Transformers\MissingDataImputer;
+use Rubix\ML\Transformers\NumericStringConverter;
+
+use function Laravel\Prompts\confirm;
 
 class HousePricePredictorCommand extends Command
 {
@@ -27,6 +39,49 @@ class HousePricePredictorCommand extends Command
      */
     public function handle()
     {
-        //
+        $logger = new Screen();
+        $extractor = new ColumnPicker(new CSV('dataset.csv', true), [
+            'MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street', 'Alley',
+            'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope',
+            'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle',
+            'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'RoofStyle',
+            'RoofMatl', 'Exterior1st', 'Exterior2nd', 'MasVnrType', 'MasVnrArea',
+            'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual', 'BsmtCond',
+            'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1', 'BsmtFinType2', 'BsmtFinSF2',
+            'BsmtUnfSF', 'TotalBsmtSF', 'Heating', 'HeatingQC', 'CentralAir',
+            'Electrical', '1stFlrSF', '2ndFlrSF', 'LowQualFinSF', 'GrLivArea',
+            'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'BedroomAbvGr',
+            'KitchenAbvGr', 'KitchenQual', 'TotRmsAbvGrd', 'Functional', 'Fireplaces',
+            'FireplaceQu', 'GarageType', 'GarageYrBlt', 'GarageFinish', 'GarageCars',
+            'GarageArea', 'GarageQual', 'GarageCond', 'PavedDrive', 'WoodDeckSF',
+            'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea',
+            'PoolQC', 'Fence', 'MiscFeature', 'MiscVal', 'MoSold', 'YrSold',
+            'SaleType', 'SaleCondition', 'SalePrice',
+        ]);
+
+        $dataset = Labeled::fromIterator($extractor);
+
+        $dataset->apply(new NumericStringConverter())
+            ->apply(new MissingDataImputer())
+            ->transformLabels('intval');
+
+        $estimator = new PersistentModel(
+            new GradientBoost(new RegressionTree(4), 0.1),
+            new Filesystem('housing.rbx', true)
+        );
+
+        $estimator->setLogger($logger);
+
+        $estimator->train($dataset);
+
+        $extractor = new CSV('progress.csv', true);
+
+        $extractor->export($estimator->steps());
+
+        $logger->info('Progress saved to progress.csv');
+
+        if (confirm("Save this model?", default: false)) {
+            $estimator->save();
+        }
     }
 }
