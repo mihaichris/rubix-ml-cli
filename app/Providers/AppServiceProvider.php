27887afa696047a832749d37b0ab62399137cf74
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-12-16 15:28:40 +0200

    remove unused boot method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,13 +10,6 @@ use Illuminate\Support\ServiceProvider;
 final class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-    }
-
-    /**
      * Register any application services.
      */
     public function register(): void
