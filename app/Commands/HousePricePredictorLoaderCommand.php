a05d77e5c0b4a4076d2fd1514311f15d03b0c922
Mihai <48624094+mihaichris@users.noreply.github.com>
2023-12-18 11:19:17 +0200

    remove doc notation mixed

--- app/Commands/HousePricePredictorLoaderCommand.php
+++ app/Commands/HousePricePredictorLoaderCommand.php
@@ -33,7 +33,6 @@ final class HousePricePredictorLoaderCommand extends Command
     /**
      * Execute the console command.
      *
-     * @return mixed
      */
     public function handle(): void
     {
